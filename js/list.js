95bb3800bb1f5b95d7c582cb70c912f23c0f14ea
BigMistake <morgan.heijdemann@gmail.com>
2018-07-27 16:29:31 +0200

    Because the list.js contentscript matches at both the list view and the individual house pages, I added a try and catch, where I've the list logic in the try and the house page logic in the catch. Also added the ability to give permission for WOZWaardeloket, which is the website I need to make web requests to in order to get the WOZ valuation.

--- js/list.js
+++ js/list.js
@@ -1,151 +1,244 @@
-function editPage(){
-    // Get the container for the search results, with the ol containers that hold the individual results.
-    let container = Array.prototype.slice.call(document.querySelectorAll('[data-instant-search-output="results"]')[0]['children']);
-    for(let i = 0; i < container.length;i++){
-        // Only the ol elements contain results, so lets get rid of the divs and uls containing ads and "recommendations".
-        if(container[i].localName === "div" || container[i].localName === "ul"){
-            chrome.storage.sync.get({
-                removeAdverts: false
-            }, function(item) {
-                if(item.removeAdverts === true){
-                    container[i].remove();
-                }
-            });
-        } else {
-            // Loop through the individual search results, being the houses.
-            for(let x = 0; x < container[i]['children'].length;x++){
-                let searchResult = container[i]['children'][x];
-
-                // Extract the price.
-                let price = searchResult.getElementsByClassName('search-result-price')[0].innerText.replace(/\./g,'').replace(/€/g,'').replace(/kk/g,'').replace(/\/mnd/g,'').replace(/von/g,'');
-                //let adres = searchResult.getElementsByClassName('search-result-title')[0].innerText;
-
-                if(window.location.href.includes("huur")){
-                    try{
-                        let prices = price.match(/.*\s(\d{1,})\stot.*/);
-                        price = prices[1];
-                    }
-                    catch{
-                        //console.log("Geen variable kosten gevonden.");
-                    }
-                }
-
-                function removeSimilar(){
-                    try{
-                        let similar = searchResult.getElementsByClassName("search-result-similar")[0];
-                        similar.remove();
-                    }
-                    catch{
-                        //console.log("No similar results found.");
-                    }
-                }
-
-                function removePromo(){
-                    try{
-                        let promotext = searchResult.getElementsByClassName("search-promo-text")[0];
-                        let promolabel = searchResult.getElementsByClassName("search-promolabel")[0];
-
-                        promotext.remove();
-                        promolabel.remove();
-                    }
-                    catch{
-                        //console.log("No promo materials found.");
-                    }
-
-                    let makelaar = searchResult.getElementsByClassName("search-result-makelaar")[0];
-                    makelaar.remove();
-                }
-
-                function showWoon(){
-                    let woonoppervlakte = "";
-
-                    try{
-                        woonoppervlakte = searchResult.querySelector('[title="Woonoppervlakte"]').innerText.replace(" m²","").replace(".","");
-                        // Create the result elements
-                        let gemiddelde = document.createElement("ul");
-                        gemiddelde.setAttribute("class","search-result-kenmerken");
-                        let gemiddeldeTekst = document.createTextNode("Woonoppervlakte prijs per m²: €" + Number(Math.round(price / woonoppervlakte)).toLocaleString('nl-NL'));
-                        gemiddelde.appendChild(gemiddeldeTekst);
-                        searchResult.children[1].children[1].children[0].children[2].appendChild(gemiddelde);
-                    }
-                    catch (err) {
-                        //console.log("Geen woonoppervlakte voor deze advertentie gevonden: " + adres);
-                    }
-                }
-
-                function showPerceel(){
-                    let perceeloppervlakte = "";
-
-                    try{
-                        perceeloppervlakte = searchResult.querySelector('[title="Perceeloppervlakte"]').innerText.replace(" m²","").replace(".","");
-                        // Create the result elements
-                        let gemiddelde = document.createElement("ul");
-                        gemiddelde.setAttribute("class","search-result-kenmerken");
-                        let gemiddeldeTekst = document.createTextNode("Perceeloppervlakte prijs per m²: €" + Number(Math.round(price / perceeloppervlakte)).toLocaleString('nl-NL'));
-                        gemiddelde.appendChild(gemiddeldeTekst);
-                        searchResult.children[1].children[1].children[0].children[2].appendChild(gemiddelde);
-                    }
-                    catch (err) {
-                        //console.log("Geen perceeloppervlakte voor deze advertentie gevonden: " + adres);
-                    }
-                }
-
+try{
+    function editPage(){
+        // Get the container for the search results, with the ol containers that hold the individual results.
+        let container = Array.prototype.slice.call(document.querySelectorAll('[data-instant-search-output="results"]')[0]['children']);
+        for(let i = 0; i < container.length;i++){
+            // Only the ol elements contain results, so lets get rid of the divs and uls containing ads and "recommendations".
+            if(container[i].localName === "div" || container[i].localName === "ul"){
                 chrome.storage.sync.get({
-                    removeSimilar: false,
-                    removePromo: false,
-                    showWoon: true,
-                    showPerceel: false,
-                }, function(items) {
-                    if(items.removeSimilar){
-                        removeSimilar();
-                    }
-                    if(items.removePromo){
-                        removePromo();
-                    }
-                    if(items.showWoon){
-                        showWoon();
-                    }
-                    if(items.showPerceel){
-                        showPerceel()
+                    removeAdverts: false
+                }, function(item) {
+                    if(item.removeAdverts === true){
+                        container[i].remove();
                     }
                 });
+            } else {
+                // Loop through the individual search results, being the houses.
+                for(let x = 0; x < container[i]['children'].length;x++){
+                    let searchResult = container[i]['children'][x];
+
+                    // Extract the price.
+                    let price = searchResult.getElementsByClassName('search-result-price')[0].innerText.replace(/\./g,'').replace(/€/g,'').replace(/kk/g,'').replace(/\/mnd/g,'').replace(/von/g,'');
+                    //let adres = searchResult.getElementsByClassName('search-result-title')[0].innerText;
+
+                    if(window.location.href.includes("huur")){
+                        try{
+                            let prices = price.match(/.*\s(\d{1,})\stot.*/);
+                            price = prices[1];
+                        }
+                        catch{
+                            //console.log("Geen variable kosten gevonden.");
+                        }
+                    }
+
+                    function removeSimilar(){
+                        try{
+                            let similar = searchResult.getElementsByClassName("search-result-similar")[0];
+                            similar.remove();
+                        }
+                        catch{
+                            //console.log("No similar results found.");
+                        }
+                    }
+
+                    function removePromo(){
+                        try{
+                            let promotext = searchResult.getElementsByClassName("search-promo-text")[0];
+                            let promolabel = searchResult.getElementsByClassName("search-promolabel")[0];
+
+                            promotext.remove();
+                            promolabel.remove();
+                        }
+                        catch{
+                            //console.log("No promo materials found.");
+                        }
+
+                        let makelaar = searchResult.getElementsByClassName("search-result-makelaar")[0];
+                        makelaar.remove();
+                    }
+
+                    function showWoon(){
+                        let woonoppervlakte = "";
+
+                        try{
+                            woonoppervlakte = searchResult.querySelector('[title="Woonoppervlakte"]').innerText.replace(" m²","").replace(".","");
+                            // Create the result elements
+                            let gemiddelde = document.createElement("ul");
+                            gemiddelde.setAttribute("class","search-result-kenmerken");
+                            let gemiddeldeTekst = document.createTextNode("Woonoppervlakte prijs per m²: €" + Number(Math.round(price / woonoppervlakte)).toLocaleString('nl-NL'));
+                            gemiddelde.appendChild(gemiddeldeTekst);
+                            searchResult.children[1].children[1].children[0].children[2].appendChild(gemiddelde);
+                        }
+                        catch (err) {
+                            //console.log("Geen woonoppervlakte voor deze advertentie gevonden: " + adres);
+                        }
+                    }
+
+                    function showPerceel(){
+                        let perceeloppervlakte = "";
+
+                        try{
+                            perceeloppervlakte = searchResult.querySelector('[title="Perceeloppervlakte"]').innerText.replace(" m²","").replace(".","");
+                            // Create the result elements
+                            let gemiddelde = document.createElement("ul");
+                            gemiddelde.setAttribute("class","search-result-kenmerken");
+                            let gemiddeldeTekst = document.createTextNode("Perceeloppervlakte prijs per m²: €" + Number(Math.round(price / perceeloppervlakte)).toLocaleString('nl-NL'));
+                            gemiddelde.appendChild(gemiddeldeTekst);
+                            searchResult.children[1].children[1].children[0].children[2].appendChild(gemiddelde);
+                        }
+                        catch (err) {
+                            //console.log("Geen perceeloppervlakte voor deze advertentie gevonden: " + adres);
+                        }
+                    }
+
+                    chrome.storage.sync.get({
+                        removeSimilar: false,
+                        removePromo: false,
+                        showWoon: true,
+                        showPerceel: false,
+                    }, function(items) {
+                        if(items.removeSimilar){
+                            removeSimilar();
+                        }
+                        if(items.removePromo){
+                            removePromo();
+                        }
+                        if(items.showWoon){
+                            showWoon();
+                        }
+                        if(items.showPerceel){
+                            showPerceel()
+                        }
+                    });
+                }
             }
         }
     }
-}
 
 //First execution, the next ones will be activated by the mutation observer
-editPage();
-let waiting = false;
+    editPage();
+    let waiting = false;
 
 // Select the node that will be observed for mutations
-let targetNode = document.getElementsByClassName("search-content-output")[0];
+    let targetNode = document.getElementsByClassName("search-content-output")[0];
 
 // Options for the observer (which mutations to observe)
-let config = {childList: true};
+    let config = {childList: true};
 
 // Callback function to execute when mutations are observed
-let callback = function(mutationsList) {
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            if(!waiting){
-                waiting = true;
-                editPage();
-                observer.disconnect();
-                window.setTimeout(function(){
-                    observer.observe(targetNode, config);
-                    waiting = false;
-                },1000);
+    let callback = function(mutationsList) {
+        for(let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                if(!waiting){
+                    waiting = true;
+                    editPage();
+                    observer.disconnect();
+                    window.setTimeout(function(){
+                        observer.observe(targetNode, config);
+                        waiting = false;
+                    },1000);
+                }
             }
         }
-    }
-};
+    };
 
 // Create an observer instance linked to the callback function
-let observer = new MutationObserver(callback);
+    let observer = new MutationObserver(callback);
 
 
 // Start observing the target node for configured mutations after initial load
-window.setTimeout(function(){
-    observer.observe(targetNode, config);
-},2000);
+    window.setTimeout(function(){
+        observer.observe(targetNode, config);
+    },2000);
+}
+catch{
+    let price = document.getElementsByClassName("object-header__price")[0].innerText.replace(/\./g,"").replace(/€/g,"").replace(/kk/g,"").replace(/\/mnd/g,"").replace(/von/g,"");
+    let adres = document.getElementsByClassName("object-header__address")[0].innerText;
+
+    if(window.location.href.includes("huur")){
+        try{
+            let prices = price.match(/.*\s(\d{1,})\stot.*/);
+            price = prices[1];
+        }
+        catch{
+            //console.log("Geen variable kosten gevonden.");
+        }
+    }
+
+    function showWoon(){
+        let woonoppervlakte = "";
+
+        try{
+            woonoppervlakte = document.querySelector('[title="Woonoppervlakte"]').innerText.replace(" m²","").replace(".","");
+            // Create the result elements
+            let gemiddelde = document.createElement("ul");
+            gemiddelde.setAttribute("class","search-result-kenmerken");
+            let gemiddeldeTekst = document.createTextNode("Woonoppervlakte prijs per m²: €" + Number(Math.round(price / woonoppervlakte)).toLocaleString('nl-NL'));
+            gemiddelde.appendChild(gemiddeldeTekst);
+            searchResult.children[1].children[1].children[0].children[2].appendChild(gemiddelde);
+        }
+        catch (err) {
+            //console.log("Geen woonoppervlakte voor deze advertentie gevonden: " + adres);
+        }
+    }
+
+    function getWOZ(){
+        let url = "https://www.wozwaardeloket.nl/api/geocoder/v2/suggest?query=" + encodeURI(adres);
+
+        $.get(url, function(data) {
+            let id = data["docs"][0]["id"];
+            let adres = data["docs"][0]["weergavenaam"];
+
+            url = "https://www.wozwaardeloket.nl/api/geocoder/v2/lookup?id=" + encodeURI(id);
+
+            $.get(url, function(details) {
+                $.ajax({
+                    type: "POST",
+                    url: "https://www.wozwaardeloket.nl/woz-proxy/wozloket",
+                    data: '<wfs:GetFeature xmlns:wfs="http://www.opengis.net/wfs" service="WFS" version="1.1.0" xsi:schemaLocation="http://www.opengis.net/wfs http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><wfs:Query typeName="wozloket:woz_woz_object" srsName="EPSG:28992"  xmlns:ogc="http://www.opengis.net/ogc"><ogc:Filter xmlns:ogc="http://www.opengis.net/ogc"><ogc:And><ogc:PropertyIsEqualTo matchCase="true"><ogc:PropertyName>wobj_bag_obj_id</ogc:PropertyName><ogc:Literal>' + details["adresseerbaarobject_id"].substr(1) + '</ogc:Literal></ogc:PropertyIsEqualTo></ogc:And></ogc:Filter></wfs:Query></wfs:GetFeature>',
+                    success: function(data){
+                        //let entry = data["features"][data["features"].length -1]["properties"];
+                        //console.log("Voor " +  adres + " is sinds " + entry["wobj_wrd_peildatum"] + " de WOZ waarde: €" + Number(entry["wobj_wrd_woz_waarde"]).toLocaleString('nl-NL'));
+                        let date = "00-00-0000";
+                        let element = 0;
+
+                        for(let i=0;i<data["features"].length;i++){
+                            let entry = data["features"][i]["properties"];
+                            if(entry["wobj_wrd_ingangsdatum"] > date){
+                                date = entry["wobj_wrd_peildatum"];
+                                element = i;
+                            }
+                        }
+
+                        let entry = data["features"][element]["properties"];
+                        console.log("Voor " +  adres + " is vanaf " + entry["wobj_wrd_ingangsdatum"] + " de WOZ waarde: €" + Number(entry["wobj_wrd_woz_waarde"]).toLocaleString('nl-NL'));
+                    }
+                });
+            });
+        });
+    }
+
+    chrome.storage.sync.get({
+        removeSimilar: false,
+        removePromo: false,
+        showWoon: true,
+        showPerceel: false,
+        getWOZ: false
+    }, function(items) {
+        if(items.removeSimilar){
+            //removeSimilar();
+        }
+        if(items.removePromo){
+            //removePromo();
+        }
+        if(items.showWoon){
+            showWoon();
+        }
+        if(items.showPerceel){
+            //showPerceel()
+        }
+        if(items.getWOZ){
+            getWOZ();
+        }
+    });
+}
--- js/options.js
+++ js/options.js
@@ -1,11 +1,9 @@
-console.log("Test");
 function save_options() {
     let removeAdverts = document.getElementById('adverts').checked;
     let removeSimilar = document.getElementById('similar').checked;
     let removePromo = document.getElementById('promo').checked;
     let showWoon = document.getElementById('woon').checked;
     let showPerceel = document.getElementById('perceel').checked;
-    let getWOZ = document.getElementById('woz').checked;
 
     chrome.storage.sync.set({
         removeAdverts: removeAdverts,
@@ -13,7 +11,6 @@ function save_options() {
         removePromo: removePromo,
         showWoon: showWoon,
         showPerceel: showPerceel,
-        getWOZ: getWOZ
     }, function() {
         let status = document.getElementById('status');
         status.textContent = 'Options saved.';
@@ -24,6 +21,63 @@ function save_options() {
     });
 }
 
+function editPermissions(){
+    function requestPermission(){
+        chrome.permissions.request({
+            origins: ["https://www.wozwaardeloket.nl/"]
+        }, function(granted) {
+            if (granted) {
+                //console.log("Toestemming verlenen is gelukt");
+                chrome.storage.sync.set({
+                    getWOZ: true
+                });
+            } else {
+                //console.log("Toestemming verlenen is niet gelukt.");
+                document.getElementById("woz").checked = false;
+            }
+        });
+    }
+
+    function removePermission(){
+        chrome.permissions.remove({
+            origins: ["https://www.wozwaardeloket.nl/"]
+        }, function(removed) {
+            if (removed) {
+                //console.log("Toestemming intrekken is gelukt.");
+                chrome.storage.sync.set({
+                    getWOZ: false
+                });
+            } else {
+                //console.log("Toestemming intrekken is niet gelukt.");
+                document.getElementById("woz").checked = true;
+            }
+        });
+    }
+
+    if(document.getElementById("woz").checked){
+        //console.log("Toestemming moet nu gevraagd worden.");
+        requestPermission();
+    }
+    else {
+        //console.log("Toestemming moet nu verwijderd worden.");
+        removePermission();
+    }
+}
+
+function checkPermissions(){
+    chrome.permissions.contains({
+        origins: ["https://www.wozwaardeloket.nl/"]
+    }, function(result) {
+        if (result) {
+            document.getElementById("woz").checked = true;
+            console.log("Toestemming gevonden.");
+        } else {
+            document.getElementById("woz").checked = false;
+            console.log("Toestemming niet gevonden.");
+        }
+    });
+}
+
 function restore_options() {
     chrome.storage.sync.get({
         removeAdverts: true,
@@ -31,17 +85,17 @@ function restore_options() {
         removePromo: false,
         showWoon: true,
         showPerceel: false,
-        getWOZ: false
     }, function(items) {
         document.getElementById('adverts').checked = items.removeAdverts;
         document.getElementById('similar').checked = items.removeSimilar;
         document.getElementById('promo').checked = items.removePromo;
         document.getElementById('woon').checked = items.showWoon;
         document.getElementById('perceel').checked = items.showPerceel;
-        document.getElementById('woz').checked = items.getWOZ;
     });
 }
 document.addEventListener('DOMContentLoaded', function () {
     restore_options();
+    checkPermissions();
+    document.querySelector('#woz').addEventListener('click', editPermissions);
     document.querySelector('#save').addEventListener('click', save_options);
 });
